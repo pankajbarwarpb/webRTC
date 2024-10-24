3d5a1a3d37e0b66159b772042cd32f3752425945
pankajbarwarpb <pankaj.barwar@amantyatech.com>
2024-10-24 17:40:17 +0530

    Added the code for connection, but not being able to send the stream

--- Starter Code/server/index.js
+++ Starter Code/server/index.js
@@ -1,42 +1,59 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
 
 const app = express();
-const io = new Server({ cors: true });
-
+const ioServer = createServer();
+const io = new Server(ioServer, {
+  cors: {
+    origin: "*",
+  },
+});
 app.use(bodyParser.json());
 
 const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
 
 io.on("connection", (socket) => {
-  console.log("New user connected with id : ", socket.id);
+  console.log("New Connection: ", socket.id);
 
   socket.on("join-room", (data) => {
-    const { roomId, emailId } = data;
-    console.log(
-      "User ",
-      emailId,
-      " joined room ",
-      roomId,
-      " from : ",
-      socket.id
-    );
-    emailToSocketMapping.set(emailId, socket.id);
+    const { emailId, roomId } = data;
+    console.log(data, { emailId, roomId });
+
     socket.join(roomId);
-    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+    emailToSocketMapping.set(emailId, socket.id);
+    socketToEmailMapping.set(socket.id, emailId);
+
+    socket.emit("joined-room", { roomId });
+    socket.broadcast.to(roomId).emit("user-joined", { emailId, roomId });
+  });
+
+  socket.on("call-user", (data) => {
+    const { emailId, offer } = data;
+    console.log("call-user", data);
+
+    const fromEmail = socketToEmailMapping.get(socket.id);
+    const socketId = emailToSocketMapping.get(emailId);
+
+    socket.to(socketId).emit("incoming-call", { from: fromEmail, offer });
+  });
+
+  socket.on("call-accepted", (data) => {
+    console.log("call-accepted", { data });
+    const { emailId, ans } = data;
+
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit("call-accepted", { ans });
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected with id : ", socket.id);
-    console.log("");
+    console.log("Disconnected :", socket.id);
   });
 });
 
-app.listen(8000, () => {
-  console.log("HTTP Server running at PORT 8000");
-});
+app.listen(8000, () => console.log("Http server running at PORT 8000"));
 
-io.listen(8001, () => {
-  console.log("Socket IO listening at PORT 8001");
-});
+ioServer.listen(8001);
+console.log("Socket-io started listening at PORT 8001");
